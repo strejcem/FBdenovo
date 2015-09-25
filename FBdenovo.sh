28e4681916900f338841cc06cb8d2d51ec950382
Dr. Uhlik laboratory of Applied Microbiology <strejcem@vscht.cz>
2015-09-25 12:11:42 +0200

    Update FBdenovo.sh
    updated usage information

--- FBdenovo.sh
+++ FBdenovo.sh
@@ -1,90 +1,139 @@
 #!/bin/bash
-# usage: refbot.sh sorted.fasta
 
-# designed for sequences trimmed to 400bp
+## v1.01
+## FrameBot de novo bash script
+## May 2015
+## by Michal Strejcek (strejcem@vscht.cz)
 
-if [ -z "$1" ]
-then
-	echo "Usage: `basename $0` [fasta file]"
-	echo "Input DNA sequences must be sorted by abundance (most = first)"
-	exit
-fi
 
-FrameBot='java -jar /home/lovec/opt/FrameBot1.0/dist/FrameBot.jar'	#FrameBot.jar path
-id=0.4	#FB identity cutoff
-len=100	#FB Aminoacid length cutoff
+# modify this to the full path to the FrameBot.jar
+RDPframebot='java -jar /home/lovec/opt/RDPTools/FrameBot.jar'
 
-	
-fastastring()
-{
-#transform fasta file from block format to single-line format	
-while read line; do
-        if [ "${line:0:1}" = ">" ]; then
-		if [ ! -z "$seq" ]; then
-			echo "$seq"
-		fi		
-		unset seq
-		echo "$line"
-	else		
+fastastring() {
+	#transform fasta file from block format to single-line format	
+	while read line; do
+        	if [ "${line:0:1}" = ">" ]; then
+			if [ ! -z "$seq" ]; then
+				echo "$seq"
+			fi		
+			unset seq
+			echo "$line"
+		else		
 			seq="${seq}${line}"
-	fi
-done
-echo "$seq"
+		fi
+	done
+	echo "$seq"
 }
 
-FBdenovo()
-{
+FBdenovo() {
+
+	echo "using $in as input file"
+	echo "FB identity treshold set to $id"
+	echo "FB min AA length is set to $len"
 	# removing results-files
-	rm -f "$1"_denovo_prot.fasta
-	rm -f "$1"_denovo_nucl.fasta
-	rm -f "$1"_ref.STOP	# .STOP file contains unprocessed sequences that contain STOP codon - often non-target sequences
+	rm -f "$in"_denovo_prot.fasta
+	rm -f "$in"_denovo_nucl.fasta
+	rm -f "$in"_ref.STOP	# .STOP file contains unprocessed sequences that contain STOP codon - often non-target sequences
 
-
-	mkdir FrameBot_$1
-	cd FrameBot_$1
+	mkdir FrameBot_$in
+	cd FrameBot_$in
 	
 	# 0th cycle 
 	i=0
-	fastastring < ../$1 > "$1".string
-	cp "$1".string STOP_check	# preparation for STOP codon check
+	fastastring < ../$in > "$in".string
+	cp "$in".string STOP_check	# preparation for STOP codon check
 	head -2 < STOP_check > temp	# translation of the most abundant sequence = "ref"
-	$FrameBot translate 11 temp ref 1
+	$RDPframebot translate 11 temp ref 1
 	rm -f "$i"_ref.STOP
 	while [[ $(tail -2 ref | grep "*" -c) > 0 ]]; do	#checks for STOP codon
 			cat temp >> "$i"_ref.STOP
 				tail -n+3 < STOP_check > STOP_check1	# skips the sequence
 			mv STOP_check1 STOP_check
 			head -2 < STOP_check > temp	# selects new reference
-			$FrameBot translate 11 temp ref 1
+			$RDPframebot translate 11 temp ref 1
 	done
 
 	# running global (seqs usually trimmed to the same length) FrameBot with the "ref" as a reference
-	$FrameBot framebot -f -15 -N -a global -l "$len" -o "$i" -i "$id" ref "$1".string
-	cat 0_corr_prot.fasta >> ../"$1"_denovo_prot.fasta
-	cat 0_corr_nucl.fasta >> ../"$1"_denovo_nucl.fasta
+	$RDPframebot framebot -f -15 -N -a global -l "$len" -o "$i" -i "$id" ref "$in".string
+	cat 0_corr_prot.fasta >> ../"$in"_denovo_prot.fasta
+	cat 0_corr_nucl.fasta >> ../"$in"_denovo_nucl.fasta
 	
 	# 1+ cycles
 	while [ -s "$i"_corr_nucl.fasta ] || [ -s ref ]; do
 		cp "$i"_failed_nucl.fasta STOP_check	
 		head -2 < STOP_check > temp	
-		$FrameBot translate 11 temp ref 1
+		$RDPframebot translate 11 temp ref 1
 		rm -f "$i"_ref.STOP
 		while [[ $(tail -2 ref | grep "*" -c) > 0 ]]; do 
 			cat temp >> "$i"_ref.STOP
 			tail -n+3 < STOP_check > STOP_check1
 			mv STOP_check1 STOP_check
 			head -2 < STOP_check > temp
-			$FrameBot translate 11 temp ref 1
+			$RDPframebot translate 11 temp ref 1
 		done
 	
-		$FrameBot framebot -f -15 -N -a global -l "$len" -o "$[$i+1]" -i "$id" ref "$i"_failed_nucl.fasta
-		cat "$[$i+1]"_corr_prot.fasta >> ../"$1"_denovo_prot.fasta
-		cat "$[$i+1]"_corr_nucl.fasta >> ../"$1"_denovo_nucl.fasta
-		cat "$i"_ref.STOP > ../"$1_"ref.STOP	# print unprocessed sequences that cotain STOP codon
+		$RDPframebot framebot -f -15 -N -a global -l "$len" -o "$[$i+1]" -i "$id" ref "$i"_failed_nucl.fasta
+		cat "$[$i+1]"_corr_prot.fasta >> ../"$in"_denovo_prot.fasta
+		cat "$[$i+1]"_corr_nucl.fasta >> ../"$in"_denovo_nucl.fasta
+		if [ -f "$i"_ref.STOP ]; then
+			cat "$i"_ref.STOP > ../"$in_"ref.STOP	# print unprocessed sequences that cotain STOP codon
+		fi
+		echo "iteration "$i""
 		i=$[$i+1]
 	done
-echo "FrameBot de novo - finished"
-echo "Sequences are NOT dereplicated"
+	
+	rm -f temp
+	rm -f ref
+
+	echo "FrameBot de novo - finished"
+	echo "Sequences are NOT dereplicated"
 }
 
-FBdenovo $1
+usage="\n$(basename "$0") -i <fasta_file> [-t <0..1>] [-l <integer>]
+Where:
+	-h  show this message
+	-i  input file (sorted fasta file; most = first)
+	-t  FrameBot internal parameter for identity threshold (default=0.4)
+	-l  FrameBot internal parameter for minimal AA length (default=100)
+
+- requires installed FrameBot https://github.com/rdpstaff/Framebot and specify the full path to it in the beginning of this script
+- the input must be sorted by sequence abundance (first is the most)
+- the input sequences should be trimmed to the same lenght (e.g. 400bp)" 
+
+in=
+id=0.4
+len=100
+while getopts "hi:tl" OPTION; do
+	case $OPTION in
+	h)
+		echo -e "$usage"
+		exit
+		;;
+	i)
+		in=$OPTARG
+		;;
+	t)
+		id=$OPTARG
+		;;
+	l)
+		len=$OPTARG     
+		;;
+	*)
+		echo -e "$usage" >&2
+		exit 1
+		;;
+	esac
+done
+if [ -z "$in" ]; then
+	echo -e "\nillegal operation"
+	echo -e "$usage" >&2
+	exit 1
+else
+	if [ ! -f "$in" ];then
+		echo -e "\nThe input file does not exist!"
+		echo -e "$usage"
+		exit 1
+fi
+fi
+
+FBdenovo
